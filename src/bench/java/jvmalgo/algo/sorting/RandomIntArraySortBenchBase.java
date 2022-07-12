a141bc101b320db64e3ffa991f71329c83a286e4
Debo <mail@deboder.nl>
2022-07-12 14:36:21 +0200

    refactored test

---
+++ src/bench/java/jvmalgo/algo/sorting/RandomIntArraySortBenchBase.java
@@ -0,0 +1,73 @@
+package jvmalgo.algo.sorting;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+@State(Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 1)
+@Measurement(iterations = 1)
+@Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"}, warmups = 1)
+@BenchmarkMode(Mode.AverageTime)
+public class RandomIntArraySortBenchBase {
+
+    private static final Random rnd = new Random();
+
+    // Array sizes to be generated ( change in 3 spots int this file if you change )
+    protected static final String SIZE_10 = "10";
+    protected static final String SIZE_100 = "100";
+    protected static final String SIZE_1000 = "1000";
+    protected static final String SIZE_10000 = "10000";
+    protected static final String SIZE_100000 = "100000";
+
+    // Arrays with random data
+    protected static final int[][] arrays =
+            IntStream
+                    .of(
+                            Arrays
+                                    .stream(new String[] {
+                                            SIZE_10,
+                                            SIZE_100,
+                                            SIZE_1000,
+                                            SIZE_10000,
+                                            SIZE_100000,
+                                    })
+                                    .mapToInt(Integer::parseInt)
+                                    .toArray()
+                    )
+                    .mapToObj(i -> IntStream.generate(rnd::nextInt)
+                            .limit(i)
+                            .toArray()
+                    )
+                    .toArray(int[][]::new);
+
+    // params given to the test
+    @Param({SIZE_10, SIZE_100, SIZE_1000, SIZE_10000, SIZE_100000})
+    protected int arraySize;
+
+
+
+
+    // convert from arraysize to it's index in 'arrays'
+    protected static int getIndex(int count) {
+        int index = 0;
+        count /= 10;
+        while ((count /= 10) != 0) {
+            index++;
+        }
+        return index;
+    }
+
+    // The actual benching
+    protected void benchAlgo(Consumer<int[]> algo, Blackhole blackhole) {
+        var arr = arrays[getIndex(arraySize)].clone();
+        algo.accept(arr);
+        blackhole.consume(arr);
+    }
+}
--- src/bench/java/jvmalgo/algo/sorting/SortBench.java
+++ src/bench/java/jvmalgo/algo/sorting/SortBench.java
@@ -2,91 +2,32 @@ package jvmalgo.algo.sorting;
 
 import jvmalgo.algos.sorting.InsertionSort;
 import jvmalgo.algos.sorting.SelectionSort;
-import org.openjdk.jmh.annotations.*;
+import jvmalgo.algos.sorting.ShellSort;
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.infra.Blackhole;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 1)
-@Measurement(iterations = 1)
-@Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"}, warmups = 1)
-public class SortBench {
-
-    static Random rnd = new Random();
-    static int[] arr10;
-    static int[] arr100;
-    static int[] arr1000;
-    static int[] arr10000;
-
-    static {
-        arr10 = Stream.generate(() -> rnd.nextInt()).mapToInt(i -> i).limit(10).toArray();
-        arr100 = Stream.generate(() -> rnd.nextInt()).mapToInt(i -> i).limit(100).toArray();
-        arr1000 = Stream.generate(() -> rnd.nextInt()).mapToInt(i -> i).limit(1000).toArray();
-        arr10000 = Stream.generate(() -> rnd.nextInt()).mapToInt(i -> i).limit(10000).toArray();
-    }
-
+public class SortBench extends RandomIntArraySortBenchBase {
 
     @Benchmark
-    public void selection10(Blackhole blackhole) {
-        var arr = arr10.clone();
-        SelectionSort.algo.accept(arr);
-        blackhole.consume(arr);
+    public void selection(Blackhole bh) {
+        benchAlgo(SelectionSort.algo, bh);
     }
 
     @Benchmark
-    public void selection100(Blackhole blackhole) {
-        var arr = arr100.clone();
-        SelectionSort.algo.accept(arr);
-        blackhole.consume(arr);
+    public void insertion(Blackhole bh) {
+        benchAlgo(InsertionSort.algo, bh);
     }
 
     @Benchmark
-    public void selection1000(Blackhole blackhole) {
-        var arr = arr1000.clone();
-        SelectionSort.algo.accept(arr);
-        blackhole.consume(arr);
+    public void shell(Blackhole bh) {
+        benchAlgo(ShellSort.algo, bh);
     }
 
     @Benchmark
-    public void selection10000(Blackhole blackhole) {
-        var arr = arr10000.clone();
-        SelectionSort.algo.accept(arr);
-        blackhole.consume(arr);
+    public void javaArraysSort(Blackhole bh) {
+        benchAlgo(Arrays::sort, bh);
     }
-
-    @Benchmark
-    public void insertion10(Blackhole blackhole) {
-        var arr = arr10.clone();
-        InsertionSort.algo.accept(arr);
-        blackhole.consume(arr);
-    }
-
-    @Benchmark
-    public void insertion100(Blackhole blackhole) {
-        var arr = arr100.clone();
-        InsertionSort.algo.accept(arr);
-        blackhole.consume(arr);
-    }
-
-    @Benchmark
-    public void insertion1000(Blackhole blackhole) {
-        var arr = arr1000.clone();
-        InsertionSort.algo.accept(arr);
-        blackhole.consume(arr);
-    }
-
-    @Benchmark
-    public void insertion10000(Blackhole blackhole) {
-        var arr = arr10000.clone();
-        InsertionSort.algo.accept(arr);
-        blackhole.consume(arr);
-    }
-
 
 }
